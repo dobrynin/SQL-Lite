ff4940b35dd9f2500ce3fd23395ec3bcad81d9c5
App Academy Student <student@appacademy.io>
2017-04-14 17:02:14 -0700

    Makes where lazy

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,17 +3,24 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    where_line = params.keys.map { |key| "#{key} = ?"}.join(' AND ')
-    results = DBConnection.execute(<<-SQL, params.values)
-      SELECT
-        *
-      FROM
-        #{table_name}
-      WHERE
-        #{where_line}
-    SQL
+    @past_searches = {} if @past_searches.nil?
+    if @past_searches.include?(params)
+      @past_searches[params]
+    else
+      where_line = params.keys.map { |key| "#{key} = ?" }.join(' AND ')
+      results = DBConnection.execute(<<-SQL, params.values)
+        SELECT
+          *
+        FROM
+          #{table_name}
+        WHERE
+          #{where_line}
+      SQL
 
-    parse_all(results)
+      objects = parse_all(results)
+      @past_searches[params] = objects
+      objects
+    end
   end
 end
 
